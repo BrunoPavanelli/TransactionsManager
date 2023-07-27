da4ce66115cb0ff6c6d768e85c9417921c2efceb
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-27 18:37:28 -0300

    feat(server): add initial configs

---
+++ back/src/app.ts
@@ -0,0 +1,16 @@
+import express, { json } from 'express';
+import "reflect-metadata"
+import "express-async-errors"
+
+import routes from './routes';
+import sharedMiddlewares from './shared';
+
+const app = express()
+app.use(json())
+
+app.use("/users", routes.users)
+app.use("/transactions", routes.transactions)
+
+app.use(sharedMiddlewares.handleError)
+
+export default app
---
+++ back/src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app";
+
+const appPort = 3000 || process.env.APP_PORT;
+
+const server = (port: number) =>
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+  });
+
+server(appPort)
