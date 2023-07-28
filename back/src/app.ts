79aedcbdfb24b0b3d6941efb1fd96fe3d9ccf420
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-28 12:41:19 -0300

    feat: implement cors for localhost permission

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import "reflect-metadata";
 import "express-async-errors";
+import cors from "cors";
 
 import routes from "./routes";
 import sharedMiddlewares from "./shared";
@@ -8,6 +9,8 @@ import sharedMiddlewares from "./shared";
 
 const app = express();
 app.use(json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/users", routes.users);
 app.use("/transactions", routes.transactions);
